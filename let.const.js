96ac472de53405a76f4344d65d875a88e0480303
Angelica <agervais97@gmail.com>
2022-03-01 21:18:05 -0500

    completed exercise

---
+++ let.const.js
@@ -0,0 +1,13 @@
+var PI = 3.14;
+PI = 42;
+
+const PI = 3.14;
+PI = 42;
+
+// The difference between var and let, Var can be redelcared multiple times while let cannot 
+
+// The difference between var and const: var can be redeclared  and reassigned  while Const cannot 
+
+// The difference between let and const: let can be reassigned while const cannot 
+
+//Hoisting - where variables can be used before they are declared 
